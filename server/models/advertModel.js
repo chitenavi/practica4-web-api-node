6911e20b567a091c8b83f45d57d81d605ea17773
Ivan Chinea <chitenavi@gmail.com>
2020-09-04 17:01:37 +0100

    Add: create Advert Model, add json file with adds

---
+++ server/models/advertModel.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const advertSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'An advert must have a name'],
+    index: true,
+    trim: true,
+  },
+  sale: {
+    type: Boolean,
+    default: true,
+    index: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'An advert must have a price'],
+  },
+  tinyDescription: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: [true, 'An advert must have an image'],
+  },
+  tags: {
+    type: [String],
+    index: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+});
+
+const Advert = mongoose.model('Advert', advertSchema);
+
+export default Advert;
