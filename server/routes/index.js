5d12b32c85fcf4d4a9480f651883242d38f6be37
Ivan Chinea <chitenavi@gmail.com>
2020-09-04 23:13:52 +0100

    add: styles, app, front-end

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,35 @@
-const express = require('express');
+import express from 'express';
+
+import Advert from '../models/advertModel';
+import APIFeatures from '../utils/apiFeatures';
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  try {
+    const features = new APIFeatures(Advert.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const adverts = await features.query;
+
+    res.render('index', { title: 'Nodepop', adverts });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* GET detail product page. */
+router.get('/adverts/:id', async (req, res, next) => {
+  // EXECUTE QUERY
+  try {
+    const advert = await Advert.findById(req.params.id);
+    res.render('advertdetail', { title: advert.name, advert });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
